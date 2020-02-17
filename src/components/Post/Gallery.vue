850ad3d29b26edc48276655c7a6c9b9d6e024964
harrisonfm <june@harrisonfm.com>
2020-02-17 13:32:08 -0700

    Init

---
+++ src/components/Post/Gallery.vue
@@ -0,0 +1,22 @@
+<template>
+	<ul>
+		<li v-for="photo in photos" :key="photo.id">
+			<img :src="photo.image.thumbnail" />
+		</li>
+	</ul>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import SETTINGS from "../../settings";
+
+export default {
+  data() {
+  },
+  props: ['photos'],
+
+  computed: {},
+
+  methods: {},
+};
+</script>
