b1044f434f642584082cc7d660a5294b0b6ee0df
harrisonfm <june@harrisonfm.com>
2020-11-25 12:14:36 -0700

    Updated Tailwind

--- webpack/loaders/postcss.js
+++ webpack/loaders/postcss.js
@@ -1,9 +1,6 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  test: /\.css$/,
-  use: ExtractTextPlugin.extract({
-    fallback: 'style-loader',
-    use: [{ loader: 'css-loader', options: { importLoaders: 1 } }, 'postcss-loader'],
-  }),
+  test: /\.css$/i,
+  use: [MiniCssExtractPlugin.loader, 'css-loader'],
 };
