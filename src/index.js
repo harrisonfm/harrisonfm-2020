31e0e2143a9985cea2710cd8897275c96162e10f
harrisonfm <john@harrisonfm.com>
2022-02-07 21:26:51 -0700

    remove extra dark mode code

--- src/index.js
+++ src/index.js
@@ -43,12 +43,6 @@ Vue.use(VueLazyload, {
 
 window.eventBus = new Vue();
 
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
-
 new Vue({
   el: '#app',
   store,
